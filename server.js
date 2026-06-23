a64533f16460577948d11b81dc7a95ffb3bf147a
sudaisnadeemhere <sudais731@gmail.com>
2026-06-23 05:32:30 -0700

    changed

--- server.js
+++ server.js
@@ -20,19 +20,13 @@ app.use(cors({
   origin: "https://trello-frontend-eta.vercel.app",
   methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
   allowedHeaders: ["Content-Type", "Authorization"],
-  credentials: true
 }));
 
-app.use((req, res, next) => {
-  res.header("Access-Control-Allow-Origin", "https://trello-frontend-eta.vercel.app");
-  res.header("Access-Control-Allow-Headers", "Content-Type, Authorization");
-  res.header("Access-Control-Allow-Methods", "GET,POST,PUT,DELETE,OPTIONS");
-  next();
-});
+
 
 // Routes
-app.use("/api/auth", authRoutes);
-app.use("/api/tasks", taskRoutes);
+app.use("/auth", authRoutes);
+app.use("/tasks", taskRoutes);
 
 app.get("/", (req, res) => {
   res.json({ message: "Trello-lite backend running" });
