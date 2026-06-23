d6518ab41114d233fe868d5bd1cb02bea03d8537
sudaisnadeemhere <sudais731@gmail.com>
2026-06-23 04:27:06 -0700

    vercel ready

--- server.js
+++ server.js
@@ -20,7 +20,4 @@ app.get('/', (req, res) => {
   res.send({ message: 'Trello-lite backend running' });
 });
 
-const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => {
-  console.log(`Server listening on port ${PORT}`);
-});
+module.exports = app;
