2570272fae13f259b760e509fe0d9fdddc62ed14
sudaisnadeemhere <sudais731@gmail.com>
2026-06-23 05:21:56 -0700

    see

--- server.js
+++ server.js
@@ -1,55 +1,33 @@
-import express from "express";
-import cors from "cors";
-import dotenv from "dotenv";
-import connectDB from "./config/db.js";
+const express = require("express");
+const cors = require("cors");
+const dotenv = require("dotenv");
 
-import authRoutes from "./routes/auth.js";
-import taskRoutes from "./routes/tasks.js";
+const connectDB = require("./config/db");
+const authRoutes = require("./routes/auth");
+const taskRoutes = require("./routes/tasks");
 
 dotenv.config();
 
 const app = express();
 
-// DB connection
-let isConnected = false;
+// DB
+connectDB();
 
-const connect = async () => {
-if (isConnected) return;
-await connectDB();
-isConnected = true;
-};
-
-connect();
-
-// CORS
-app.use(
-cors({
-origin: "https://trello-frontend-eta.vercel.app",
-methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
-allowedHeaders: ["Content-Type", "Authorization"],
-credentials: true,
-})
-);
-
-// Body parsers
+// Middlewares
 app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
 
-// Extra CORS headers
+app.use(cors({
+  origin: "https://trello-frontend-eta.vercel.app",
+  methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+  allowedHeaders: ["Content-Type", "Authorization"],
+  credentials: true
+}));
+
 app.use((req, res, next) => {
-res.header(
-"Access-Control-Allow-Origin",
-"https://trello-frontend-eta.vercel.app"
-);
-res.header(
-"Access-Control-Allow-Headers",
-"Content-Type, Authorization"
-);
-res.header(
-"Access-Control-Allow-Methods",
-"GET,POST,PUT,DELETE,OPTIONS"
-);
-next();
+  res.header("Access-Control-Allow-Origin", "https://trello-frontend-eta.vercel.app");
+  res.header("Access-Control-Allow-Headers", "Content-Type, Authorization");
+  res.header("Access-Control-Allow-Methods", "GET,POST,PUT,DELETE,OPTIONS");
+  next();
 });
 
 // Routes
@@ -57,7 +35,7 @@ app.use("/api/auth", authRoutes);
 app.use("/api/tasks", taskRoutes);
 
 app.get("/", (req, res) => {
-res.send({ message: "Trello-lite backend running" });
+  res.json({ message: "Trello-lite backend running" });
 });
 
-export default app;
+module.exports = app;
