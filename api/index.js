53d568236cf1ace9767eae64b01360350894152c
sudaisnadeemhere <sudais731@gmail.com>
2026-06-24 02:54:46 -0700

    solve

--- api/index.js
+++ api/index.js
@@ -11,6 +11,7 @@ const allowedOrigins = [
   "http://localhost:3000",
   "http://127.0.0.1:5173",
   "http://127.0.0.1:3000",
+  "http://localhost:5000",
   "https://to-do-application-frontend-phi.vercel.app",
   process.env.FRONTEND_URL,
   ...(process.env.CORS_ORIGINS?.split(",") || []),
